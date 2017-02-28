d66a1f812dcbb970d2d3e5d7b18815849edf564d
Tom Price <tom_p@hotmail.co.uk>
2017-02-28 10:26:07 +0900

    Append SVG circles

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,10 @@
 
 function renderScatterPlot(data) {
-    console.log(parseSeconds(2387));
-    // set up svg and bar dimensions/margins
-    const width = 1000;
-    const height = 500;
-    const padding = 20;
-    const margin = { top: 40, right: 20, bottom: 80, left: 80 };
+  console.log(data[0]);
+  // set up svg and bar dimensions/margins
+  const width = 800;
+  const height = 500;
+  const margin = { top: 40, right: 20, bottom: 80, left: 80 };
 
   // append svg to DOM
   const svg = d3.select('body')
@@ -13,32 +12,31 @@ function renderScatterPlot(data) {
     .attr('width', width)
     .attr('height', height)
 
+  // for parsing time string in data and converting back to original format
+  const parse = d3.timeParse('%M:%S');
+  const format = d3.timeFormat('%M:%S')
+
   // calculate min and max data values
-  const timeMin = d3.min(data, (d) => parseSeconds(d.Seconds));
-  const timeMax = d3.max(data, (d) => parseSeconds(d.Seconds));
+  const timeMin = d3.min(data, (d) => parse(d.Time));
+  const timeMax = d3.max(data, (d) => parse(d.Time));
 
   // set up scales
   const xScale = d3.scaleBand()
     .domain(data.map((d) => d.Year))
     .range([margin.left, width - margin.right])
     .paddingInner(0.1)
+    .paddingOuter(0.5)
 
   const yScale = d3.scaleTime()
     .domain([timeMin, timeMax])
     .range([height - margin.bottom, margin.top])
 
-  // filter data to space out x axis tick points - 1 per data point is too many
-  const tickValues = xScale.domain().filter((d, i) => !(i % 2));
-
   // set up x and y axes
   const xAxis = d3.axisBottom(xScale)
-    // .tickValues(tickValues)
 
-  const yAxis = d3.axisLeft(yScale);
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat(format)
 
-  // // calculate x and y positions for axis labels
-  // const xLabelPosition = width / 2 + margin.left;
-  // const yLabelPosition = height / 2 - margin.bottom;
 
   // append axes to svg
   svg.append('g')
@@ -47,30 +45,21 @@ function renderScatterPlot(data) {
     .call(xAxis)
 
   svg.append('g')
-    .call(d3.axisLeft(yScale))
+    .call(yAxis)
     .attr('transform', `translate(${margin.left}, 0)`)
     .attr('id', 'y-axis')
-}
 
-// function that sets bar width and padding
-function setBarWidth(width, data, paddingMultiplier) {
-  const barWidth = width / data.length;
-  const barWidthPadding = barWidth * paddingMultiplier;
-
-  return barWidth - barWidthPadding;
-}
-
-// function for parsing and formatting dates in data
-function parseAndFormat(date) {
-  const parsed = d3.timeParse('%Y-%m-%d')(date)
-  return formatted = d3.timeFormat('%Y')(parsed);
-}
-
-function parseSeconds(seconds) {
-  const m = seconds / 60;
-  const s = seconds % 60;
-  const date = new Date(2016, 01, 01, 01, m, s);
-  return d3.timeFormat("%M: %S")(date);
+  svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', (d) => xScale(d.Year))
+    .attr('cy', (d) => yScale(parse(d.Time)))
+    .attr('r', 3)
+    .style('fill', (d) => {
+      if (d.Doping) return 'blue';
+      else return 'orange';
+    })
 }
 
 $.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json', (data) => renderScatterPlot(data));
